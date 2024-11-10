524d1226d16bc493b6471a31aa1c6e59e2c7ced3
PIYUSH PRAKASH PUJARI <59538413+piyushprakashpujari@users.noreply.github.com>
2024-11-10 18:39:09 +0000

    date

---
+++ 01_Basics/date.js
@@ -0,0 +1,13 @@
+let myDate=new Date();
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toISOString());
+
+let myCratedDate= new Date(2023,0,23);
+console.log(myCratedDate);
+console.log(myCratedDate.toString());
+console.log(myCratedDate.toDateString());
+console.log(myCratedDate.toLocaleString());
+console.log(myCratedDate.toISOString());
--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,11 @@
 console.log('piyush')
+let min=10;
+let max=20;
+
+let a =Math.random();
+let b=a*10;
+let c=Math.floor(b);
+let d=Math.floor(a*(max-min+1))+min;
+
+
+console.log(a,b,c,d);
